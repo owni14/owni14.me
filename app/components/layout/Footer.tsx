2a59aee56935627270aaa60ea1cf61d4728f2e69
owni14 <kmwoo514@gmail.com>
2024-03-25 00:51:19 +0900

    [Style] header and footer style

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,5 +1,28 @@
+import dayjs from "dayjs";
+import Github from "@public/images/github.svg";
+import Blog from "@public/images/blog.svg";
+import Mail from "@public/images/mail.svg";
+import Link from "next/link";
+
 const Footer = () => {
-    return <div id="footer">footer.tx</div>;
+    const currentYear = dayjs().year();
+
+    return (
+        <div id="footer">
+            <p>{`© ${currentYear}. Minwoo Kim. All rights reserved.`}</p>
+            <div className="link-area">
+                <Link href="https://github.com/owni14">
+                    <Github />
+                </Link>
+                <Link href="https://owni14.github.io/">
+                    <Blog />
+                </Link>
+                <Link href="mailto:kmwoo514@gmail.com">
+                    <Mail />
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
     webpack: config => {
         config.resolve.alias["@app"] = path.resolve(process.cwd(), "app");
         config.resolve.alias["@public"] = path.resolve(process.cwd(), "public");
+        config.module.rules.push({
+            test: /\.svg$/i,
+            use: ["@svgr/webpack"],
+        });
         return config;
     },
 };
