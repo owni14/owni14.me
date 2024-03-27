dcda2670e5d0c42704afe0fa01e5b1cf26f317bd
owni14 <kmwoo514@gmail.com>
2024-03-28 00:08:48 +0900

    [Style] About 제작 완료(반응형 적용 전)

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -8,12 +8,15 @@ const About = () => {
             <Image src={Me} alt="me.png" />
             <div className="text-wrapper">
                 <div className="link-key">About</div>
-                <p className="main-text">{"안녕하세요!\n저는 프론트엔드 개발자 김민우입니다."}</p>
-                <p className="sub-text">
-                    {
-                        "항상 사용자 입장에 서서 UI/UX측면에서 더 좋은 성능을 내기위해 노력하고 있으며,\n디자이너 및 기획자와 끊임없이 소통을 하고 있습니다."
-                    }
-                </p>
+                <div className="text-area">
+                    <p className="text">{"안녕하세요!\n저는 프론트엔드 개발자 김민우입니다."}</p>
+                    <p className="text">{"긍정적 사고와 책임감을 바탕으로 즐기면서 개발하고 있습니다."}</p>
+                    <p className="text">
+                        {
+                            "조금씩 기록을 남기며 아는 부분과 모르는 부분을 회고하는 시간을 가지고 있으며 내용 공유를 통해 함께 성장하자는 마인드를 가지고 있습니다."
+                        }
+                    </p>
+                </div>
             </div>
         </div>
     );
