e32acb93e5f90cf2801773d343bf47f196ef4a36
owni14 <kmwoo514@gmail.com>
2024-03-31 02:42:58 +0900

    [Fix] next config basePath 추가

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    basePath: "/portfolio",
     webpack: config => {
         config.resolve.alias["@app"] = path.resolve(process.cwd(), "app");
         config.resolve.alias["@public"] = path.resolve(process.cwd(), "public");
