a39bba3c0beacf248b17b07ffeba53107e145364
owni14 <kmwoo514@gmail.com>
2024-04-06 12:37:28 +0900

    [Fix] 초기 언어 설정 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <html lang="en">
+        <html lang="ko">
             <body>
                 <Layout>{children}</Layout>
             </body>
