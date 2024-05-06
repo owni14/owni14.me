0759f6d0e3792b5dccbf007445d769574485e1e0
owni14 <kmwoo514@gmail.com>
2024-05-06 18:47:01 +0900

    init: folder structure

---
+++ src/plugins/locales/index.ts
@@ -0,0 +1,9 @@
+import * as ko from "./ko";
+import * as en from "./en";
+
+interface localeType {
+  [key: string]: { [key: string]: string | { [key: string]: string } };
+}
+
+export const localeKR: localeType = { ...ko };
+export const localeEN: localeType = { ...en };
