31bedeb6bf87e0cd356d2fc6f20380f2470c19c7
owni14 <kmwoo514@gmail.com>
2024-06-05 01:58:06 +0900

    style: about 스크롤할때 opacity 스타일 추가

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import cx from "classnames";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 
 import Me from "@/public/images/me.png";
@@ -12,17 +14,54 @@ import { SKILLS } from "./consts";
 
 const About = () => {
   const { t } = useTranslation(NAMESPACE.ABOUT);
+  const sectionRefs = useRef<(HTMLElement | null)[]>([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          const element = entry.target as HTMLElement;
+          if (entry.isIntersecting) {
+            element.style.opacity = "1";
+          } else {
+            element.style.opacity = "0";
+          }
+        });
+      },
+      { threshold: 0.55 }
+    );
+
+    sectionRefs.current.forEach(ref => {
+      observer.observe(ref as HTMLElement);
+    });
+
+    return () => {
+      if (observer) {
+        observer.disconnect();
+      }
+    };
+  }, []);
 
   return (
     <section id="about">
-      <div className="introduce-wapper">
+      <section
+        className={cx("introduce-wapper", { isVisible: true })}
+        ref={el => {
+          sectionRefs.current[0] = el;
+        }}
+      >
         <Image src={Me} alt="me" />
         <div className="introduce-sentence">
           <p className="introduce-sentence">{t("topIntro")}</p>
           <p className="introduce-sentence">{t("midIntro")}</p>
         </div>
-      </div>
-      <div className="skill-wrapper">
+      </section>
+      <section
+        className="skill-wrapper"
+        ref={el => {
+          sectionRefs.current[1] = el;
+        }}
+      >
         <div className="icon-wrapper">
           {SKILLS.map(skill => {
             const SkillIcon = skill.icon;
@@ -34,7 +73,7 @@ const About = () => {
             );
           })}
         </div>
-      </div>
+      </section>
     </section>
   );
 };
