34a5cadc556489b95942c5b728ec11116889a8df
owni14 <kmwoo514@gmail.com>
2024-06-18 23:08:55 +0900

    fix: lint오류로 인한 재배포 테스트

--- app/_components/ui/card/types.ts
+++ app/_components/ui/card/types.ts
@@ -1,11 +1,11 @@
 import { StaticImageData } from "next/image";
 
 export interface ICard {
-    src: StaticImageData;
-    alt: string;
-    title: string;
-    desc: string;
-    tags: string[];
-    link: string;
-    code: string;
+  src: StaticImageData;
+  alt: string;
+  title: string;
+  desc: string;
+  tags: string[];
+  link: string;
+  code: string;
 }
